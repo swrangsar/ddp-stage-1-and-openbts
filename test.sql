41031ee8a768d285d1fa7dd75124d69ce283fc11
swrangsar basumatary <swrangsar@Inspiron-1564.(none)>
2013-08-07 19:12:43 +0530

    commit at 20130807191243

---
+++ test.sql
@@ -0,0 +1,4 @@
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404929509230948', '7777', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404929509230948');
+INSERT INTO dialdata_table (exten, dial) VALUES ('7777', 'IMSI404929509230948');
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404201572311737', '5555', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404201572311737');
+INSERT INTO dialdata_table (exten, dial) VALUES ('5555', 'IMSI404201572311737');
