e5658e22a350ce4a59915bd8b2b740a9c5b6775b
swrangsar basumatary <swrangsar@Inspiron-1564.(none)>
2013-08-07 19:48:53 +0530

    commit at 20130807194853

--- test.sql
+++ test.sql
@@ -1,4 +1,2 @@
-INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404929509230948', '7777', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404929509230948');
-INSERT INTO dialdata_table (exten, dial) VALUES ('7777', 'IMSI404929509230948');
-INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404201572311737', '5555', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404201572311737');
-INSERT INTO dialdata_table (exten, dial) VALUES ('5555', 'IMSI404201572311737');
+INSERT INTO sip_buddies (name, callerid, canreinvite, type, context, allow, host, dtmfmode, ipaddr, port, username) VALUES ('IMSI404929510095344', '3333', 'no', 'friend', 'sip-external', 'gsm', 'dynamic', 'info', '127.0.0.1', '5062', 'IMSI404929510095344');
+INSERT INTO dialdata_table (exten, dial) VALUES ('3333', 'IMSI404929510095344');
