13aae2208c62aad1b4085038f3ab781ae8671fca
Swrangsar Basumatary <swrangsarb@gmail.com>
2013-09-03 00:56:10 +0530

    commit at 20130903005610

--- startBTSatFrequency.py
+++ startBTSatFrequency.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 
 from gnuradio import gr, eng_notation
-#from gnuradio.eng_option import eng_option
-#from optparse import OptionParser
 import sys
 import math
 import struct
-#import threading
 import time
 import sqlite3
 import os
@@ -14,17 +11,11 @@ from datetime import datetime
 
 def main_loop():
     usage = "usage: %prog channel_freq"
-#    parser = OptionParser(option_class=eng_option, usage=usage)
- 
-#    (options, args) = parser.parse_args()
-#    if len(args) != 1:
-#        parser.print_help()
     if len(sys.argv) != 2:
         print 'usage: ', sys.argv[0], ' channel_freq'
         sys.exit(1)
 
     center_freq = eng_notation.str_to_num(sys.argv[1])
-    print sys.argv[1], len(sys.argv)
     startOpenBTS(center_freq)
 
 def startOpenBTS(frequency):            
